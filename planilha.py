696012a739c8013d7734bcf1c5961b8c28c5c436
Marcos Sousa <marcospauloda.s.s@gmail.com>
2023-07-10 12:02:19 -0300

    Atualizar planilha.py

--- planilha.py
+++ planilha.py
@@ -1,6 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from gspread_formatting import cellFormat, color, format_cell_range, batch_updater
+from gspread_formatting import cellFormat, color, format_cell_range, textFormat, batch_updater
 import time
 
 scope = ['https://spreadsheets.google.com/feeds',
@@ -28,7 +28,7 @@ def readTask():
     pessoasAlteradas = []
     for l in range(1, len(info)):
         p = info[l][1]
-        for c in range(2, len(info[0])-1):
+        for c in range(2, len(info[0])-3):
             if info[l][c] != '':
                 for n in info[l][c].split(", "):
                     número = 10*(c-2) + int(n)
@@ -39,7 +39,7 @@ def readTask():
         pessoas[p].sort()
     return pessoasAlteradas
 
-def updateTask(force=False):
+def updateTask(force=False, reset=-1):
     credenciais = []
     planilha = []
     gc = []
@@ -48,26 +48,32 @@ def updateTask(force=False):
     batch = []
 
     pessoasAlteradas = readTask()
+    if reset != -1:
+        pessoas[list(pessoas.keys())[reset]] = []
 
     for w in range(3):
         credenciais.append(ServiceAccountCredentials.from_json_keyfile_name(f'worker{w+1}.json'
                 , scope))
-         
+
         gc.append(gspread.authorize(credenciais[w]))
         planilha.append(gc[w].open('Rifa de 100 pontos'))
         pagf.append(planilha[w].get_worksheet(0))
         pag.append(planilha[w].get_worksheet(1))
         batch.append(batch_updater(pag[w].spreadsheet))
-    
+
     infof = pagf[0].get_all_values()
     info2 = pag[0].get_all_values()
 
-    verde = cellFormat(
-    backgroundColor=color(.85, .917, .827)
+    verdinho = cellFormat(
+    backgroundColor=color(.576, .768, .49),
+    textFormat=textFormat(bold=False, foregroundColor=color(1, 1, 1))
     )
     vermelho = cellFormat(
-    backgroundColor=color(.956, .8, .8)
+    backgroundColor=color(.85, .917, .827),
+    textFormat=textFormat(bold=False, foregroundColor=color(0, 0, 0))
     )
+
+
 
     #Atualizar coisas na segunda planilha
     ii = 0
@@ -80,10 +86,10 @@ def updateTask(force=False):
                         break
                     pessoa = list(pessoas.keys())[ii]
                     #pag.update_cell(l+1, c+1, pessoa)
-                    
+
                     if pessoa in pessoasAlteradas or force:
                         for i in range(100):
-                            cor = verde if int(str(ii*100 + i+1)[-2::]) in pessoas[pessoa] else vermelho
+                            cor = verdinho if int(str(ii*100 + i+1)[-2::]) in pessoas[pessoa] else vermelho
                             coord = f"{alfabeto[c + i%10]}{l+2 + i//10}" if c + i%10 < 26 else f"A{alfabeto[c + i%10-26]}{l+2 + i//10}"
                             for w in range(3):
                                 if ii >= w*15 and ii < 15+w*15:
